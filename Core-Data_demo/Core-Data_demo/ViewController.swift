f1873828520b9ebe5b60689b9b398bf579052add
Pavel <19paul91@mail.ru>
2016-06-20 11:20:05 +0300

    add files default

---
+++ Core-Data_demo/Core-Data_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core-Data_demo
+//
+//  Created by Павел Анплеенко on 20/06/16.
+//  Copyright © 2016 Pavel Anpleenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
