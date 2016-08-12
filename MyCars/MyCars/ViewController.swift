69085dab7ad172dc2d09536b5f371bde26d57fa1
Pavel <19paul91@mail.ru>
2016-08-12 08:10:14 +0300

    add files to project "MyCar"

--- MyCars/MyCars/ViewController.swift
+++ MyCars/MyCars/ViewController.swift
@@ -2,24 +2,49 @@
 //  ViewController.swift
 //  MyCars
 //
-//  Created by Павел Анплеенко on 12/08/16.
-//  Copyright © 2016 Pavel Anpleenko. All rights reserved.
+//  Created by Ivan Akulov on 27/11/15.
+//  Copyright © 2015 Ivan Akulov. All rights reserved.
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
+    
+    var managedContext: NSManagedObjectContext!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var markLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var lastTimeStartedLabel: UILabel!
+    @IBOutlet weak var numberOfTripsLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var myChoiceImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func segmentedCtrlPressed(sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func startEnginePressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func rateItPressed(sender: UIButton) {
+        
+    }
 }
 
